45ffdedf4020617c30f5321485e0f60aa899fcf4
Andrew W. Hill <andrew@textile.io>
2020-02-16 14:00:06 -0700

    fixes delete step (#3)
    Signed-off-by: andrew <andrew@textile.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "Removing Bucket $1"
 time=$(date)
 mkdir -p $HOME/.textile
 
-echo "token: $3" > $HOME/.textile/auth.yml
+echo "token: $2" > $HOME/.textile/auth.yml
 
 echo -ne '\n' | textile buckets rm $1
 wait
